d1fa47a53a80098aea9129f39c80b0c65225be81
Aymeric Blot <blot.aymeric@gmail.com>
2024-05-28 14:43:06 +0200

    simplify

--- src/app/tile-list/tile-list.component.ts
+++ src/app/tile-list/tile-list.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tile-list',
   templateUrl: './tile-list.component.html',
   styleUrl: './tile-list.component.scss'
 })
-export class TileListComponent implements OnInit {
-  drinks:any;
+export class TileListComponent {
+  drinks = [
+    {id: 0, name: 'Allongé', emoji: '☕💧', bg: '#ece0d1', counter: 0},
+    {id: 1, name: 'Café au lait', emoji: '☕🥛', bg: '#dbc1ac', counter: 0},
+    {id: 2, name: 'Petit café', emoji: '☕😐', bg: '#967259', counter: 0},
+    {id: 3, name: 'Double', emoji: '☕☕', bg: '#634832', counter: 0},
+    {id: 4, name: 'Chocolat', emoji: '☕🍫', bg: '#7b3700', counter: 0},
+    {id: 5, name: 'Thé', emoji: '🫖🍵', bg: '#b0ce84', counter: 0},
+  ]
 
-  constructor(public dataService: DataService) { }
+  constructor() { }
 
-  ngOnInit() {
-    this.drinks = this.dataService.getDrinks();    
-  }
-
-  public addToDrink(drink:any) {
-    this.dataService.addToDrink(drink.id);
+  public addToDrink(drink:{id:number}) {
+    this.drinks[drink.id].counter += 1;
     return false;
   }
 
-  public removeFromDrink(drink:any) {
-    this.dataService.removeFromDrink(drink.id);
+  public removeFromDrink(drink:{id:number}) {
+    if (this.drinks[drink.id].counter > 0) {
+      this.drinks[drink.id].counter -= 1;
+    }
     return false;
   }
 }
