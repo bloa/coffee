6d1ed314899bc06c533ba43628364382c693d295
Aymeric Blot <blot.aymeric@gmail.com>
2024-05-28 17:09:55 +0200

    add total

--- src/app/tile-list/tile-list.component.ts
+++ src/app/tile-list/tile-list.component.ts
@@ -13,18 +13,21 @@ export class TileListComponent {
     {id: 3, name: 'Double', emoji: '☕☕', bg: '#634832', counter: 0},
     {id: 4, name: 'Chocolat', emoji: '☕🍫', bg: '#7b3700', counter: 0},
     {id: 5, name: 'Thé', emoji: '🫖🍵', bg: '#b0ce84', counter: 0},
-  ]
+  ];
+  total = 0;
 
   constructor() { }
 
   public addToDrink(drink:{id:number}) {
     this.drinks[drink.id].counter += 1;
+    this.total += 1;
     return false;
   }
 
   public removeFromDrink(drink:{id:number}) {
     if (this.drinks[drink.id].counter > 0) {
       this.drinks[drink.id].counter -= 1;
+      this.total -= 1;
     }
     return false;
   }
